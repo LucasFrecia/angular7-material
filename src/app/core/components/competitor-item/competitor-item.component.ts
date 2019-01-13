65781c6ca36ca949e01ef3f76e3b814c2ff6490b
Da Man <lfrecia@debian.fibertel.com.ar>
2019-01-13 15:01:55 -0300

    added competitor item animation and localstorage populate on init

--- src/app/core/components/competitor-item/competitor-item.component.ts
+++ src/app/core/components/competitor-item/competitor-item.component.ts
@@ -6,12 +6,27 @@ import {
   Output
 } from '@angular/core';
 import { Competitor } from '../../models/competitor';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 @Component({
   selector: 'app-competitor-item',
   templateUrl: './competitor-item.component.html',
   styleUrls: ['./competitor-item.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('items', [
+      transition(':enter', [
+        style({ transform: 'scale(0.5)', opacity: 0 }),
+        animate('1s cubic-bezier(.8, -0.6, 0.2, 1.5)',
+          style({ transform: 'scale(1)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
 
 export class CompetitorItemComponent {
--- src/app/views/competitors/competitor-view/competitor-view.component.ts
+++ src/app/views/competitors/competitor-view/competitor-view.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Competitor } from '../../../core/models/competitor';
 import { Store } from '@ngrx/store';
 import * as fromCompetitorsStore from '../store';
 import * as competitorsActions from '../store/actions/competitors.actions';
+import { DestroySubscribers } from '../../../core/decorators/destroy-subscribers.decorator';
 
 @Component({
   selector: 'app-competitor-view',
@@ -12,14 +13,24 @@ import * as competitorsActions from '../store/actions/competitors.actions';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class ComponentViewComponent implements OnInit {
+@DestroySubscribers()
+export class ComponentViewComponent implements OnInit, OnDestroy {
   competitors$: Observable<Competitor[]>;
+  private subscribers: any = {};
 
   constructor(public store: Store<fromCompetitorsStore.State>) {}
 
   ngOnInit() {
     // subscribe to the store and select competitors array
     this.competitors$ = this.store.select(fromCompetitorsStore.getEntitiesArray);
+
+    // populate localstorage
+    this.subscribers.updatedTrip$ = this.store
+    .select(fromCompetitorsStore.getEntites)
+    .subscribe(competitors => {
+      localStorage.setItem('competitors_ids', JSON.stringify(Object.keys(competitors)));
+      localStorage.setItem('competitors', JSON.stringify(competitors));
+    });
   }
 
   addCompetitor(/*competitor: Competitor*/) {
@@ -35,5 +46,9 @@ export class ComponentViewComponent implements OnInit {
     if (r) {
       this.store.dispatch(new competitorsActions.DeleteCompetitor(competitor));
     }
+  }
+
+  ngOnDestroy() {
+    // DO NOT ERASE METHOD, NEEDED FOR UNSUBSCRIPTIONS
   }
 }
