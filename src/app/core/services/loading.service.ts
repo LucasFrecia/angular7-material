9fbc2725cb0c18396b44e3429cb00e657ba27463
Da Man <lfrecia@debian.fibertel.com.ar>
2019-01-13 12:47:19 -0300

    Added loading service to core

---
+++ src/app/core/services/loading.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+  })
+export class LoadingService {
+  isLoading: boolean;
+  loadingChange$ = new BehaviorSubject<boolean>(false);
+
+  constructor() {
+    this.loadingChange$.next(false);
+  }
+
+  /**
+   * Use to show or hide loading bar on header
+   * @data type: boolean
+   */
+  changeLoadingStatus(data: boolean) {
+      console.log(data);
+    this.isLoading = data;
+    this.loadingChange$.next(this.isLoading);
+  }
+}
