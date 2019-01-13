d8528bd47213ef5fec234962f531f01e5d8a0c46
Da Man <lfrecia@debian.fibertel.com.ar>
2019-01-13 12:48:53 -0300

    Added shared material module and singleton service module provider to core

---
+++ src/app/core/modules/core.module.ts
@@ -0,0 +1,41 @@
+import {
+  ModuleWithProviders,
+  NgModule,
+  Optional,
+  SkipSelf
+} from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from './material.module';
+
+import { LoadingService } from '../services/loading.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
+  declarations: [],
+  exports: [
+    MaterialModule
+  ],
+  providers: [
+    LoadingService
+  ]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only'
+      );
+    }
+  }
+
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: [{ provide: 'loading', useClass: LoadingService }]
+    };
+  }
+}
---
+++ src/app/core/modules/material.module.ts
@@ -0,0 +1,53 @@
+import {NgModule} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule,
+        MatButtonModule,
+        MatCardModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatSelectModule,
+        MatProgressBarModule,
+        MatAutocompleteModule} from '@angular/material';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatDialogModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+@NgModule({
+  imports: [
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatProgressBarModule,
+    MatAutocompleteModule,
+    MatSnackBarModule,
+    MatDividerModule,
+    MatListModule,
+    MatDialogModule
+  ],
+  declarations: [],
+  exports: [
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatProgressBarModule,
+    MatAutocompleteModule,
+    MatDividerModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
